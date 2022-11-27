8ef4a32c87dd2ec8b168b9bb1a218f264ffc14ee
Shivang Purrohit <104918730+Shivang-purrohit@users.noreply.github.com>
2022-11-27 18:12:49 +0530

    updated

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://ecomrouter.shivangpurohit.repl.co'
+export const BASE_URL ='https://ecomrouter.shivangpurohit.repl.co'
 
 
 /* https://ecomrouter.shivangpurohit.repl.co */
